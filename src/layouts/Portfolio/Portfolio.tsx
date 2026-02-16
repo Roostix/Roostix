983fcc9afb435b836e8e759a20e3873a36c343fc
Octavio Mendoza <octa9484@gmail.com>
2026-02-16 15:28:27 -0600

    eliminacion de buttons

--- src/layouts/Portfolio/Portfolio.tsx
+++ src/layouts/Portfolio/Portfolio.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { useTranslation } from "@/contexts/TranslationContext";
 
 export default function Portfolio() {
--- src/layouts/Section/Section.tsx
+++ src/layouts/Section/Section.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+
 import { useTranslation } from "@/contexts/TranslationContext";
 import './sunrise.css'
 
